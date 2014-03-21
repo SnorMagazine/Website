ee490a06e0a292e143865ca5ef7355a02c3c9fd5
unsalted <nick@unsalted.nu>
2014-03-21 18:42:41 +0100

    update config, fix mistake
    Signed-off-by: unsalted <nick@unsalted.nu>

--- php/site/config/config.php
+++ php/site/config/config.php
@@ -44,7 +44,7 @@ c::set('url', '/');
  
 */
 
-c::set('url', 'http://kirby-rhcloud.com');
+c::set('url', 'http://kirby-snor.rhcloud.com');
 
 
 /* 
@@ -89,7 +89,7 @@ http://yourdomain.com/index.php/about
 
 */
 
-c::set('rewrite', false);
+c::set('rewrite', true);
 
 
 /* 
